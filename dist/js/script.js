f74ce0a57f30284806ca375c4babbb25644848a6
“Marion” <marionacp@gmail.com>
2017-08-11 11:36:33 -0400

    cargando mejoras de movies

--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,6 +12630,42 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
+$(document).ready(function() {
+	//solo letras numeros @ y . en el input
+	$(document).on('keypress', '#email-input', function (event) {
+	    var regex = new RegExp("^[a-z0-9@.]+$");
+	    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+	    if (!regex.test(key)) {
+	        event.preventDefault();
+	        return false;
+	    }
+	});
+	$("#create-account").on("click", function(e){
+	    var name = $("#name-input").val();
+	    var username = $("#username-input").val();
+	    var email = $("#email-input").val();
+	    var country = $("#country-input").val();
+
+	    if( name == "" || username == "" || email == "" || country == "" ){
+	        alert("No puede ingresar un campo vacío")
+	    }
+	    if( email.indexOf('@') == -1 ){
+	        alert("Ingrese un correo válido");
+	        e.preventDefault();
+	    }
+	    if( name != "" && 
+	    	username != "" && 
+	    	country != "" && 
+	    	email.indexOf('@') != -1 ){
+	    	localStorage.setItem("name", name);
+	    	localStorage.setItem("username", username);
+	    	localStorage.setItem("email", email);
+	        localStorage.setItem("country", country);
+	    } else {
+	        e.preventDefault();
+	    }
+	});
+});
 $(document).ready(function(){
 
 //INICIO LOGIN//
@@ -12806,3 +12842,31 @@ $(document).ready(function(){
 	});
 
 })
+$(document).ready(function() {
+	// mostrar datos de usuario desde Localstorage
+	usernameStorage();
+	nameStorage();
+	countryStorage();
+});
+
+function usernameStorage() {
+	if (localStorage.username != "") {
+		$("#username").html(localStorage.username);
+	} else {
+		$("#username").html("Heisenberg");
+	}
+}
+function nameStorage() {
+	if (localStorage.name != "") {
+		$("#name").html(localStorage.name);
+	} else {
+		$("#name").html("Walter White");
+	}
+}
+function countryStorage() {
+	if (localStorage.country != "") {
+		$("#country").html(localStorage.country);
+	} else {
+		$("#country").html("Middle of the desert");
+	}
+}
